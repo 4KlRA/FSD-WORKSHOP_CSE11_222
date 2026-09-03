413479589aa718b0d9604a651498388613e0b2e1
AKIRA <1akiraforp@gmail.com>
2026-09-03 06:52:19 +0530

    Backend & Frontend

---
+++ Front-end/SOVA/src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+    function AboutUs() {
+    return (
+        <section id="about" className="content-section">
+        <p className="tag">ABOUT US</p>
+        <h2>About SOVA</h2>
+        <p>
+            About Us content will be added here soon.
+        </p>
+        </section>
+    );
+    }
+
+    export default AboutUs;
---
+++ Front-end/SOVA/src/components/Home.jsx
@@ -0,0 +1,19 @@
+    import "./Home.css";
+
+    function Home() {
+    return (
+        <section className="home">
+        <p className="home-tag">WELCOME TO SOVA</p>
+
+        <h1>Build better experiences, simply.</h1>
+
+        <p className="home-description">
+            A clean React application with everything you need in one place.
+        </p>
+
+        <button className="home-button">Get Started</button>
+        </section>
+    );
+    }
+
+    export default Home;
