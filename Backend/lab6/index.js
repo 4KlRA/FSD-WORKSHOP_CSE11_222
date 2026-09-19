07f71f25197b25809bd7bc326cb584863599d918
AKIRA <1akiraforp@gmail.com>
2026-09-19 17:15:44 +0530

    Frontend & Backend Connection

--- Backend/lab6/index.js
+++ Backend/lab6/index.js
@@ -2,12 +2,11 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 
-dotenv.config()
-
 const port = 3000
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 const array = [
     {
         id: 1,
---
+++ Front-end/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Home from './components/Home'
+import About from './components/About.jsx'
+import ApiTester from './components/ApiTester.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+      <ApiTester/>
+    </>
+  )
+}
+
+export default App
